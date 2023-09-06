211596746b2fa497be3029df6ffe3af86eb317a5
alit644 <101087513+alit644@users.noreply.github.com>
2023-09-06 14:13:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">ali talib</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+}
+
+export default App;
