b247a09c67235c986775010bf43507e1f89d6c6b
alit644 <101087513+alit644@users.noreply.github.com>
2023-09-06 18:06:40 +0300

    react router add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Html from './pages/Html';
+import Css from './pages/Css';
+import Javascript from './pages/Javascript';
+
+const router = createBrowserRouter([
+  {
+    path: "/Home",
+    element: <Home />,
+    errorElement: <h1 className='errorPara'>not available</h1>,
+  },
+  {
+    path: "/Html",
+    element: <Html />,
+  },
+  {
+    path: "/Css",
+    element: <Css />,
+  },
+  {
+    path: "/Javascript",
+    element: <Javascript />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
